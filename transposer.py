5c0a690b7f8ed48a8a333e05f078b47aea630b22
Tony Aldridge <zaragopha@hotmail.com>
2012-11-24 21:46:50 +0000

    Split Transposer into a class

---
+++ transposer.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python
+# -*- coding: UTF8 -*-
+
+class Transposer(object):
+    _table = {
+        u'A':'A',
+        u'Б':'B',
+        u'В':'W',
+        u'Г':'G',
+        u'Д':'D',
+        u'Е':'YE',
+        u'Ё':'YO',
+        u'Ж':'J',
+        u'З':'Z',
+        u'И':'I',
+        u'Й':'I',
+        u'К':'K',
+        u'Л':'L',
+        u'М':'M',
+        u'Н':'N',
+        u'О':'O',
+        u'Ө':'O',
+        u'П':'P',
+        u'Р':'R',
+        u'С':'S',
+        u'Т':'T',
+        u'У':'U',
+        u'Ү':'U',
+        u'Ф':'F',
+        u'Х':'KH',
+        u'Ц':'TS',
+        u'Ч':'CH',
+        u'Ш':'SH',
+        u'Щ':'SHCH',
+        u'ъ':'',
+        u'ы':'IH',
+        u'ь':'',
+        u'Э':'E',
+        u'Ю':'YU',
+        u'Я':'YA',
+        u'а':'a',
+        u'б':'b',
+        u'в':'w',
+        u'г':'g',
+        u'д':'d',
+        u'е':'ye',
+        u'ё':'yo',
+        u'ж':'j',
+        u'з':'z',
+        u'и':'i',
+        u'й':'i',
+        u'к':'k',
+        u'л':'l',
+        u'м':'m',
+        u'н':'n',
+        u'о':'o',
+        u'ө':'o',
+        u'п':'p',
+        u'р':'r',
+        u'с':'s',
+        u'т':'t',
+        u'у':'u',
+        u'ү':'u',
+        u'ф':'f',
+        u'х':'kh',
+        u'ц':'ts',
+        u'ч':'ch',
+        u'ш':'sh',
+        u'щ':'shch',
+        u'э':'e',
+        u'ю':'yu',
+        u'я':'ya'
+    }
+
+    def lookup(self, char):
+        if char in self._table:
+            return self._table[char]
+        return char
+
+    def transpose_string(self, input_string):
+        return ''.join(map(self.lookup, list(input_string)))
